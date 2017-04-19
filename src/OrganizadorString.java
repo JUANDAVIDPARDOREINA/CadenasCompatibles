70f1d0f5620968053fb87f16bdc4bb5cde4b003b
JuanDavidPardo <juan23.pa@hotmil.com>
2017-04-18 23:47:50 -0500

    Creamos metodo para desmarcar anterior.
    Este desmarca el origen y si anterior no encontró camino lo desmarca y
    sigue con otro.

--- src/OrganizadorString.java
+++ src/OrganizadorString.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,19 +21,57 @@ public class OrganizadorString {
 
     }
 
-    public static boolean busqueda (Nodo origen, List<Nodo> lista){
+    public static String busquedaNodo(String valor, ArrayList<Nodo>lista, ArrayList<Nodo>listaResultado){
+        int puntero = 0;
+        String resultado = "";
+
+        for(int i = 0; i < lista.size(); i++){
+            if (lista.get(i).getValue().equals(valor)){
+                puntero = i;
+            }
+        }
+
+        for(int i = puntero+1; i < lista.size(); i++){
+            if (!lista.get(i).isMarca()){
+                resultado = lista.get(i).getValue();
+            }
+        }
+
+        if(resultado.equals("")){
+            for(int i = 0; i < lista.size(); i++){
+                if (lista.get(i).getValue().equals(listaResultado.get(listaResultado.size()-1))){
+                    lista.get(i).desmmarcar();
+                    puntero = i;
+                }
+            }
+
+            for(int i = puntero+1; i < lista.size(); i++){
+                if (!lista.get(i).isMarca()){
+                    resultado = lista.get(i).getValue();
+                }
+            }
+
+        }
+
+        return resultado;
+    }
+
+    public static boolean busqueda (Nodo origen, ArrayList<Nodo> lista, ArrayList<Nodo> listaResultado){
         boolean bandera = false;
+
 
         for (int i = 0; i < lista.size(); i++) {
             if (!lista.get(i).isMarca()){
                 if (validar(origen.getValue(), lista.get(i).getValue())){
                     lista.get(i).marcar();
-                    busqueda(lista.get(i), lista);
+                    listaResultado.set((listaResultado.size()+1), lista.get(i));
+                    busqueda(lista.get(i), lista, listaResultado);
                     bandera = true;
                 }
             }
         }
-
+        x x
+        1 2 3 4 5 6
         if (!bandera){
             origen.desmmarcar();
             for (int i = 0; i < lista.size(); i++) {
@@ -40,13 +79,18 @@ public class OrganizadorString {
             }
         }
 
-        return true;
+        if (lista.size() == listaResultado.size())
+            return true;
+        else
+            return false;
+
     }
 
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        List<Nodo> nodos = new List<Nodo>;
+        ArrayList<Nodo> lista            = new ArrayList<Nodo>();
+        ArrayList<Nodo> listaResultado   = new ArrayList<Nodo>();
 
 
         validar("ababababa", "ababccaba");
